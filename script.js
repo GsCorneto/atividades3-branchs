9bded386d46278438c19fb954c4ef4a0a05ee9ee
GsCorneto <guilherme.corneto@gmail.com>
2024-10-26 22:57:54 -0400

    FIX: mult-button
    Corrigido o botão de multiplicação da aplicação

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const num1 = document.getElementById("i1")
 const num2 = document.getElementById("i2")
 
-const mult = function(){
+const multiplicação = function(){
     let total = num1 * num2
     let out = document.getElementById("total")
     out.innerHTML = total
